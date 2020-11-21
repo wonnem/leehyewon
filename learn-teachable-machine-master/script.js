130162b591c7018f9f9c60120337e9757c21ee60
wonnem <wonstar0314@gmail.com>
2020-11-21 15:18:14 +0900

    index.htlm update 2

---
+++ learn-teachable-machine-master/script.js
@@ -0,0 +1,71 @@
+const icon = document.querySelector('.icon');
+const on = document.querySelector('.fas.fa-video');
+const off = document.querySelector('.fas.fa-video-slash');
+const camera = document.querySelector('.camera');
+const name = document.querySelector('.name');
+const percent = document.querySelector('.percent');
+const description = document.querySelector('.description');
+
+const URL = './model/';
+let model, webcam, labelContainer, maxPredictions;
+
+icon.addEventListener('click', async () => {
+  on.classList.toggle('invisible');
+  off.classList.toggle('invisible');
+
+  if (on.classList.length === 2) {
+    await webcam.pause();
+  } else {
+    await webcam.play();
+    window.requestAnimationFrame(loop);
+  }
+});
+
+async function init() {
+  const modelURL = URL + 'model.json';
+  const metadataURL = URL + 'metadata.json';
+
+  model = await tmImage.load(modelURL, metadataURL);
+  maxPredictions = model.getTotalClasses();
+
+  const flip = true;
+  webcam = new tmImage.Webcam(480, 480, flip);
+  await webcam.setup();
+
+  camera.appendChild(webcam.canvas);
+}
+
+async function loop() {
+  webcam.update();
+  await predict();
+  window.requestAnimationFrame(loop);
+}
+
+init();
+
+async function predict() {
+  const prediction = await model.predict(webcam.canvas);
+
+  for (let i = 0; i < maxPredictions; i++) {
+    const className = prediction[i].className;
+    const probability = prediction[i].probability.toFixed(2) * 100;
+
+    if (probability >= 75) {
+      if (name.innerHTML !== className) {
+        name.innerHTML = className;
+
+        if (className === 'Me') {
+          description.innerHTML = 'Me에 대한 설명을 적어볼까요?';
+        }
+
+        if (className === 'None') {
+          description.innerHTML = 'None에 대한 설명을 적어볼까요?';
+        }
+      }
+
+      if (percent.innerHTML !== probability + '%') {
+        percent.innerHTML = probability + '%';
+      }
+    }
+  }
+}
